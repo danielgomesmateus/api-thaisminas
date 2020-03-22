154408715b232b8ecb42ffaf6f1368929c5e8cf1
Daniel Gomes <daniel.gomes@provafacilnaweb.com.br>
2020-03-21 21:13:09 -0300

    Adicionado validações e configurado throttle_rates

--- apithaisminas/apithaisminas/contacts/serializers.py
+++ apithaisminas/apithaisminas/contacts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Contact
 
+import re
+
 class ContactSerializer(serializers.ModelSerializer):
     
     class Meta:
@@ -11,3 +13,22 @@ class ContactSerializer(serializers.ModelSerializer):
             'phone', 
             'message'
         )
+
+    def validate_name(self, value):
+        regex =  "[a-zA-Z]+$"
+
+        if re.search(regex, value):
+            return value
+        raise serializers.ValidationError('O valor deste campo é inválido.')
+
+    def validate_phone(self, value):
+        regex = "\(\d{2}\)\d{5}-\d{4}"
+
+        if re.search(regex, value):
+            return value
+        raise serializers.ValidationError('O valor deste campo é inválido. Use o formato (99)99999-9999.')
+
+    def validate_message(self, value):
+        if len(value) >= 10:
+            return value
+        raise serializers.ValidationError('O valor deste campo é muito curto.')
--- apithaisminas/apithaisminas/settings.py
+++ apithaisminas/apithaisminas/settings.py
@@ -140,5 +140,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'public/images/')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '100/minute'
+    }
 }
